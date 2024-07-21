6219c94ce74c243fc60be7c832ac2819c55d9d60
TarikSoğukpinar <tariksogukpinar@outlook.com>
2024-07-21 04:51:21 +0300

    updated auth github & google stratey

--- api/src/auth-github/strategies/auth-github.strategy.ts
+++ api/src/auth-github/strategies/auth-github.strategy.ts
@@ -9,7 +9,7 @@ export class GitHubStrategy extends PassportStrategy(Strategy, 'github') {
     super({
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/github/callback',
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
       scope: ['user:email'],
     });
   }
--- api/src/auth-google/strategies/google.strategy.ts
+++ api/src/auth-google/strategies/google.strategy.ts
@@ -8,7 +8,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth-google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       scope: ['email', 'profile'],
     });
   }
